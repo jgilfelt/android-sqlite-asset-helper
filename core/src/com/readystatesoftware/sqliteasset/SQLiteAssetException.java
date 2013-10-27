39e11cbdf4c265d7f72df86802864f2fb6b2bf8d
jgilfelt <jgilfelt@gmail.com>
2013-10-27 22:47:47 +0000

    suppresswarning

--- core/src/com/readystatesoftware/sqliteasset/SQLiteAssetException.java
+++ core/src/com/readystatesoftware/sqliteasset/SQLiteAssetException.java
@@ -21,6 +21,7 @@ import android.database.sqlite.SQLiteException;
 /**
 * An exception that indicates there was an error with SQLite asset retrieval or parsing.
 */ 
+@SuppressWarnings("serial")
 public class SQLiteAssetException extends SQLiteException {
 	
 	public SQLiteAssetException() {}
