82a5ad7fe1f88cf6294eb0702186f6454ceb8940
jgilfelt <jgilfelt@gmail.com>
2014-01-16 19:10:43 +0000

    rename example modules

---
+++ samples/database-v1/src/main/java/com/example/MyDatabase.java
@@ -0,0 +1,43 @@
+package com.example;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
+
+import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
+
+public class MyDatabase extends SQLiteAssetHelper {
+
+	private static final String DATABASE_NAME = "northwind";
+	private static final int DATABASE_VERSION = 1;
+	
+	public MyDatabase(Context context) {
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);
+		
+		// you can use an alternate constructor to specify a database location 
+		// (such as a folder on the sd card)
+		// you must ensure that this folder is available and you have permission
+		// to write to it
+		//super(context, DATABASE_NAME, context.getExternalFilesDir(null).getAbsolutePath(), null, DATABASE_VERSION);
+		
+	}
+
+	public Cursor getEmployees() {
+
+		SQLiteDatabase db = getReadableDatabase();
+		SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
+
+		String [] sqlSelect = {"0 _id", "FirstName", "LastName"}; 
+		String sqlTables = "Employees";
+
+		qb.setTables(sqlTables);
+		Cursor c = qb.query(db, sqlSelect, null, null,
+				null, null, null);
+
+		c.moveToFirst();
+		return c;
+
+	}
+
+}
---
+++ samples/database-v2-upgrade/src/main/java/com/example/MyDatabase.java
@@ -0,0 +1,49 @@
+package com.example;
+
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
+
+import com.readystatesoftware.sqliteasset.SQLiteAssetHelper;
+
+public class MyDatabase extends SQLiteAssetHelper {
+
+	private static final String DATABASE_NAME = "northwind";
+	private static final int DATABASE_VERSION = 2;
+	
+	public MyDatabase(Context context) {
+		super(context, DATABASE_NAME, null, DATABASE_VERSION);	
+		
+		// you can use an alternate constructor to specify a database location
+		// (such as a folder on the sd card)
+		// you must ensure that this folder is available and you have permission
+		// to write to it
+		//super(context, DATABASE_NAME, context.getExternalFilesDir(null).getAbsolutePath(), null, DATABASE_VERSION);
+
+        // call this method to force a database overwrite every time the version number increments:
+        //setForcedUpgrade();
+
+		// call this method to force a database overwrite if the version number
+		// is below a certain threshold:
+		//setForcedUpgrade(2);
+	}
+
+	public Cursor getEmployees() {
+
+		SQLiteDatabase db = getReadableDatabase();
+		SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
+
+		String [] sqlSelect = {"0 _id", "FullName"}; 
+		String sqlTables = "Employees";
+
+		qb.setTables(sqlTables);
+		Cursor c = qb.query(db, sqlSelect, null, null,
+				null, null, null);
+
+		c.moveToFirst();
+		return c;
+
+	}
+
+}
